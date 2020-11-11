c0ab6a554dd9fe441dd5e3137b5453263ddc020c
André <andreciprianosousa@gmail.com>
2020-11-11 14:58:19 +0000

    Added search fuction on keyboard - login activity

--- app/src/main/java/pt/atp/bobi/LoginActivity.kt
+++ app/src/main/java/pt/atp/bobi/LoginActivity.kt
@@ -1,14 +1,16 @@
 package pt.atp.bobi
 
-import android.app.AppComponentFactory
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity(){
 
@@ -17,6 +19,21 @@ class LoginActivity : AppCompatActivity(){
         setContentView(R.layout.activity_login)
 
         //--------- Setup - Active components------------
+
+        // "Done" on keyboard search
+        findViewById<EditText>(R.id.editTextPassword).setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val check = areCredentialsValid()
+                if(check != "false"){
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("username", check)
+                    startActivity(intent)
+                    finish()
+                }
+                return@OnEditorActionListener true
+            }
+            false
+        })
 
         // Authenticate Button
         findViewById<Button>(R.id.btn_auth).setOnClickListener{
@@ -39,13 +56,13 @@ class LoginActivity : AppCompatActivity(){
     }
 
     private fun areCredentialsValid(): String{
-        val username = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+        val username = findViewById<EditText>(R.id.editTextUsername).text.toString()
         if(username.isEmpty()){
             findViewById<TextView>(R.id.tv_error).text = getString(R.string.error_credentials_empty_username)
             findViewById<TextView>(R.id.tv_error).visibility = View.VISIBLE
             return "false"
         }
-        val password = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+        val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
         if(password.isEmpty()){
             findViewById<TextView>(R.id.tv_error).text = getString(R.string.error_credentials_empty_password)
             findViewById<TextView>(R.id.tv_error).visibility = View.VISIBLE
